d6922d7ed4e871e73426e90caccf44fe01ec0e48
Gladiatech-Project <support@gladiatec.com>
2019-01-31 17:58:44 +0530

    Branch Modified

--- DownloadLossCDR.php
+++ DownloadLossCDR.php
@@ -1,5 +1,5 @@
 <?php
-// output headers so that the file is downloaded rather than displayed. This is For Ravindranath Tagore......
+// output headers so that the file is downloaded rather than displayed. This is For Ravindranath Tagore......are bhai Tagore nehi Rathore haii...
 header('Content-Type: text/csv; charset=utf-8');
 header('Content-Disposition: attachment; filename=data.csv');
 
